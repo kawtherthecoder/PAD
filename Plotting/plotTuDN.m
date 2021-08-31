d6094e93808632246f4536d65f6367c5a846d1e8
Kawther Rouabhi <43021608+kawtherthecoder@users.noreply.github.com>
2021-08-31 15:15:34 -0500

    Update and rename plotTuDN.m to Plotting/plotTuDN.m

--- Plotting/plotTuDN.m
+++ Plotting/plotTuDN.m
@@ -1,10 +1,12 @@
+% plotTuDN.m
+% Plots e^(N/R) vs. Turbidity for all processed photos saved in paired_data
 function plotTuDN(paired_data)
 ind = 1;
 n = 0;
 total = 0;
 for i = 1:length(paired_data)
     if paired_data(i).panelsel_flag == 1
-        metric(ind,1) = log(paired_data(i).panel_info(1).meanValueN / paired_data(i).panel_info(1).meanValueR);
+        metric(ind,1) = exp(paired_data(i).panel_info(1).meanValueN / paired_data(i).panel_info(1).meanValueR);
         tu(ind,1) = paired_data(i).tu;
         if day(paired_data(i).photodate) == 13
             c(ind,1) = 1;
@@ -22,6 +24,6 @@ end
 figure;
 scatter(tu,metric,[],c,'filled')
 colormap jet
-xlabel('Turbidity (NTU)'); ylabel('ln(NIR/R)');
+xlabel('Turbidity (NTU)'); ylabel('exp(NIR/R)');
 disp(sprintf('n = %d',n));
 disp(sprintf('total photos seen = %d',total));
