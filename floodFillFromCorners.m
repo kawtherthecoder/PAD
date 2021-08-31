c73ce6c08ce654bdd858ba570847d3f360b2b6f0
Kawther Rouabhi <43021608+kawtherthecoder@users.noreply.github.com>
2021-08-31 14:43:28 -0500

    Update floodFillFromCorners.m

--- floodFillFromCorners.m
+++ floodFillFromCorners.m
@@ -1,28 +1,12 @@
+% floodFillFromCorners.m
+% Uses corners of rectangle selected by user and returns the shape filled as a binary mask in the image
+% Inputs:
+% RGN -- image
+% xy -- array of corner coordinates (top left, top right, bottom left, bottom right)
+% Outputs:
+% maskedImage -- binary mask of shape RGN, 1 where shape is filled, 0 else
 function maskedImage = floodFillFromCorners(RGN, xy)
 
 maskedImage = false(size(RGN));
 
 maskedImage(xy(1,2):xy(3,2),xy(1,1):xy(2,1)) = 1;
-
-% % Convert RGB image into L*a*b* color space.
-% X = rgb2lab(RGB);
-% 
-% % Create empty mask.
-% BW = false(size(X,1),size(X,2));
-% 
-% % Flood fill
-% row = round(xy(2));
-% column = round(xy(1));
-% for i=1:3
-%     normX{i} = sum((X - X(row,column,i)).^2,3);
-%     normX{i} = mat2gray(normX{i});
-%     %addedRegion{i} = grayconnected(normX{i}, row, column, tolerance);
-%     BW_band{i} = BW | addedRegion{i};
-% end
-% % Now take intersection of flood fill in each band
-% 
-% BW=BW_band{1} & BW_band{2} & BW_band{3};
-% % Create masked image.
-% maskedImage = RGB;
-% maskedImage(repmat(~BW,[1 1 3])) = 0;
-% 
