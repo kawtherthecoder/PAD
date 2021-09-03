1ba45b4727c39ddb3f7db052cc960ab65e1c66e1
Kawther Rouabhi <43021608+kawtherthecoder@users.noreply.github.com>
2021-09-03 12:11:17 -0500

    Update makeDataTable.m

--- makeDataTable.m
+++ makeDataTable.m
@@ -1,14 +1,21 @@
-function [train,test] = makeDataTable(refRGNTu)
+% makeDataTable.m
 % FIRST run plotTuNormRef to get all updated refRGNTu (reflectance values)
+% Creates training and testing datasets (stratified random sampling, ratio of dates 8/12 and 8/13 are the same for train and test) from the RGN reflectances and turbidities that have been processed
+% Inputs:
+% refRGNTu -- structure containing each processed image's reflectance values and paired turbidity value
+% Outputs:
+% train -- table containing training data, including column of averaged reflectances
+% test -- table containing testing data, including column of averaged reflectances
+function [train,test] = makeDataTable(refRGNTu)
 R = [];
 G = [];
 N = [];
 AVG = [];
 Tu = [];
-Day = [];
+Day = []; % August 12th (lake athabasca) or August 13th (richardson)
 train_perc = 0.8;
 for i = 1:length(refRGNTu)
-    R(length(R)+1,1) = refRGNTu(i).refR;
+    R(length(R)+1,1) = refRGNTu(i).refR; % columns of testing and training data
     G(length(G)+1,1) = refRGNTu(i).refG;
     N(length(N)+1,1) = refRGNTu(i).refN;
     AVG(length(AVG)+1,1) = mean([R(length(R),1),G(length(G),1),N(length(N),1)]);
@@ -19,12 +26,12 @@ end
 % test/train split -- stratified random sampling
 ind = 1:length(R);
 rng 'shuffle'
-ind = ind(randperm(length(ind)));
-testind = []; trainind = [];
+ind = ind(randperm(length(ind))); % create random indexing
+testind = []; trainind = []; % indexes of refRGNTu that will be generated for training or testing data
 
-aug12test = round(numel(find(Day == 12))/length(Day)*(1-train_perc)*length(Day));
+aug12test = round(numel(find(Day == 12))/length(Day)*(1-train_perc)*length(Day)); % the number of samples in the testing data that should be 8/12 samples (the remaining number should be the number of 8/13 samples)
 for i = ind
-    if length(testind) < aug12test && Day(i) == 12
+    if length(testind) < aug12test && Day(i) == 12 % add to training/testing data based on date
         testind(length(testind)+1,1) = i;
     elseif length(testind) < aug12test && Day(i) == 13
         trainind(length(trainind)+1) = i;
@@ -35,7 +42,7 @@ for i = ind
     end
 end
 
-r = R(trainind); g = G(trainind); n = N(trainind); tu = Tu(trainind);
+r = R(trainind); g = G(trainind); n = N(trainind); tu = Tu(trainind); % create columns of final tables
 avg = AVG(trainind);
 %train = table(r,g,n,tu);
 train = table(r,g,n,avg,tu);
